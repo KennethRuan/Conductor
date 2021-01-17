3832a798146a7f83a5ad53dff63014983d850dcf
yeahbennou <38169671+yeahbennou@users.noreply.github.com>
2021-01-17 00:47:54 -0500

    Fix invalid movements

--- mouseMovement.py
+++ mouseMovement.py
@@ -22,17 +22,19 @@ def mouseMovement(curX, curY, command):
     if keyboard.is_pressed('z'):
         print("i got here")
         sys.exit()
-    if abs(numpy.sqrt(pow(prevX - curX, 2) + pow(prevY - curY, 2)) > 8):
-        pyautogui.moveTo(curX, curY)
-        print(pyautogui.position())
-    elif abs(numpy.sqrt(pow(missedX - curX, 2) + pow(missedY - curY, 2)) > 8) and check == True:
-        pyautogui.moveTo(curX, curY)
-        print(pyautogui.position())
-        check = False
-    else:
-        check = True
-        missedX = curX
-        missedY = curY
+
+    if curX != -1:  # Index finger not detected
+        if abs(numpy.sqrt(pow(prevX - curX, 2) + pow(prevY - curY, 2)) > 8):
+            pyautogui.moveTo(curX, curY)
+            print(pyautogui.position())
+        elif abs(numpy.sqrt(pow(missedX - curX, 2) + pow(missedY - curY, 2)) > 8) and check == True:
+            pyautogui.moveTo(curX, curY)
+            print(pyautogui.position())
+            check = False
+        else:
+            check = True
+            missedX = curX
+            missedY = curY
 
     if command == prevCmd:
         cmdCount += 1
@@ -40,16 +42,16 @@ def mouseMovement(curX, curY, command):
         prevCmd = command
         cmdCount = 0
 
-    # if cmdCount == 5:
-    #     if command != prevRegisteredCmd:
-    #         if command == 'l':
-    #             pyautogui.mouseDown(button='left')
-    #         elif command == 'r':
-    #             pyautogui.mouseDown(button='right')
-    #         elif command == 'n':
-    #             pyautogui.mouseUp()
-    #     prevRegisteredCmd = command
-    #     cmdCount = 0
+    if cmdCount == 5:
+        if command != prevRegisteredCmd:
+            if command == 'l':
+                pyautogui.mouseDown(button='left')
+            elif command == 'r':
+                pyautogui.mouseDown(button='right')
+            elif command == 'n':
+                pyautogui.mouseUp()
+        prevRegisteredCmd = command
+        cmdCount = 0
 
     prevX = curX
     prevY = curY
